2904b9bbf60f937bdebfbcdf9f39d015a7561f9a
Federico Schermi <federicoschermi60@gmail.com>
2023-03-18 18:46:45 +0100

    General improvements in ControllerMain class (Controller)

--- src/main/java/it/polimi/ingsw/control/ControllerMain.java
+++ src/main/java/it/polimi/ingsw/control/ControllerMain.java
@@ -3,6 +3,12 @@ package it.polimi.ingsw.control;
 import it.polimi.ingsw.model.Game;
 
 public class ControllerMain {
-    private static Game game = new Game();
+    //private static Game game = new Game();
+
+    private Game game;
+
+    public ControllerMain(Game game) {
+        this.game = game;
+    }
 
 }
