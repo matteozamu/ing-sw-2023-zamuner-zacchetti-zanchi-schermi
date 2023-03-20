7419dc086cb3f1d6b262beb85cdad1b07e551827
simone zacchetti <simone.zacchetti@mail.polimi.it>
2023-03-20 23:39:03 +0100

    aggiunta di una bozza del metodo update per il ControllerMain

--- src/main/java/it/polimi/ingsw/control/ControllerMain.java
+++ src/main/java/it/polimi/ingsw/control/ControllerMain.java
@@ -5,6 +5,9 @@ import it.polimi.ingsw.model.ObjectCard;
 import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.Shelf;
 
+import java.util.Observable;
+
+// se lo chiamiamo GameController ???
 public class ControllerMain {
     //private static Game game = new Game();
 
@@ -18,6 +21,24 @@ public class ControllerMain {
         this.game = game;
     }
      */
+
+    //update function, funzione update vista dal prof
+    //ipotizzo che dalla view arrivi una stringa Usarname
+    //ricopio la funzione con observer e observable //TODO da sostituire
+    public String update(Observable o, String username) {   // si può ritornare una stringa alla view???
+        if (username instanceof String) {
+            if (game.isUsernameAvailable(username)) {   // controllando che la stringa non sia null posso togliere il controllo nel model
+                game.addPlayer(username);
+                return "Utente inserito";
+            } else {
+                return "Nome non disponibile";
+            }
+
+        } else {
+            /* Unknown event */
+            return "Inserire un nome";
+        }
+    }
 
     // check userName and if it is available, add the player to the game
     public void checkAddPlayer() {
