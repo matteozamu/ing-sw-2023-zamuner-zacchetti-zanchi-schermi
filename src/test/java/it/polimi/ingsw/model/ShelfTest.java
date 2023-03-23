4377387f2793dacd0aa909a61ef3e184e469ab63
Federico Schermi <federicoschermi60@gmail.com>
2023-03-23 10:20:42 +0100

    Added testCheckFull method in ShelfTest class

--- src/test/java/it/polimi/ingsw/model/ShelfTest.java
+++ src/test/java/it/polimi/ingsw/model/ShelfTest.java
@@ -1,32 +1,42 @@
 package it.polimi.ingsw.model;
 
 import junit.framework.TestCase;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ShelfTest extends TestCase {
     private Shelf s;
 
-    @BeforeAll
+    @BeforeEach
     public void setUp() {
         s = new Shelf();
     }
 
     @Test
-    public void testGetNextAvailableRow(){
-        //TODO
+    public void testGetNextAvailableRow() {
+        // TODO
     }
 
     @Test
     public void testAddObjectCard() {
-        //TODO
+        // TODO
     }
 
     @Test
     public void testCheckFull() {
-        //TODO
+        // Assumiamo che la Shelf sia inizialmente vuota.
+        assertFalse(s.getFull());
+
+        // Riempire la Shelf con carte oggetto.
+        for (int row = 0; row < 6; row++) {
+            for (int col = 0; col < 5; col++) {
+                ObjectCard card = new ObjectCard(ObjectCardType.gatto, 0);
+                assertTrue(s.addObjectCard(col, card));
+            }
+        }
+
+        // Verifica che la Shelf sia piena.
+        assertTrue(s.getFull());
     }
-
-
 
 }
