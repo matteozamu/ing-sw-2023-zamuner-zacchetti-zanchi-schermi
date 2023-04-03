c9a6841745448e9e97fda5bc5d156e608134c29e
matteozamu <matteoz2001@gmail.com>
2023-04-04 00:33:57 +0200

    format code test

--- src/test/java/it/polimi/ingsw/model/ShelfTest.java
+++ src/test/java/it/polimi/ingsw/model/ShelfTest.java
@@ -28,49 +28,49 @@ public class ShelfTest extends TestCase {
             this.oc = new ObjectCard(ObjectCardType.gatto, 0);
             s.getGrid().put(new Coordinate(row, 0), this.oc);
         }
-        assertEquals(6-cards, this.s.getAvailableRows(0));
+        assertEquals(6 - cards, this.s.getAvailableRows(0));
     }
-
-        @Test
-        //TODO: da rivedere il test. Il metodo è ok
-        public void testCloseObjectCardsPoints() {
-            // 0 close object cards
-            for (int row = 0; row < s.ROWS; row++) {
-                for (int col = 0; col < s.COLUMNS; col++) {
-                    s.getGrid().put(new Coordinate(row, col), new ObjectCard(ObjectCardType.randomObjectCardType(), 1));
-                }
+    
+    @Test
+    //TODO: da rivedere il test. Il metodo è ok
+    public void testCloseObjectCardsPoints() {
+        // 0 close object cards
+        for (int row = 0; row < s.ROWS; row++) {
+            for (int col = 0; col < s.COLUMNS; col++) {
+                s.getGrid().put(new Coordinate(row, col), new ObjectCard(ObjectCardType.randomObjectCardType(), 1));
             }
-            assertEquals(0, s.closeObjectCardsPoints());
-
-            // 3 close object cards
-            ObjectCardType type = ObjectCardType.gatto;
-            s.getGrid().put(new Coordinate(1, 1), new ObjectCard(type, 2));
-            s.getGrid().put(new Coordinate(1, 2), new ObjectCard(type, 1));
-            s.getGrid().put(new Coordinate(1, 3), new ObjectCard(type, 0));
-            assertEquals(2, s.closeObjectCardsPoints());
-
-            // 4 close object cards
-            s.getGrid().put(new Coordinate(1, 0), new ObjectCard(type, 2));
-            assertEquals(3, s.closeObjectCardsPoints());
-
-            // 5 close object cards
-            s.getGrid().put(new Coordinate(0, 2), new ObjectCard(type, 1));
-            assertEquals(5, s.closeObjectCardsPoints());
-
-            // 6 close object cards
-            s.getGrid().put(new Coordinate(1, 2), new ObjectCard(type, 0));
-            assertEquals(8, s.closeObjectCardsPoints());
-
-            // 7 close object cards
-            s.getGrid().put(new Coordinate(2, 2), new ObjectCard(type, 1));
-            assertEquals(8, s.closeObjectCardsPoints());
-
-            // empty shelf
-            for (int row = 0; row < s.ROWS; row++) {
-                for (int col = 0; col < s.COLUMNS; col++) {
-                    s.getGrid().remove(new Coordinate(row, col));
-                }
-            }
-            assertEquals(0, s.closeObjectCardsPoints());
         }
+        assertEquals(0, s.closeObjectCardsPoints());
+
+        // 3 close object cards
+        ObjectCardType type = ObjectCardType.gatto;
+        s.getGrid().put(new Coordinate(1, 1), new ObjectCard(type, 2));
+        s.getGrid().put(new Coordinate(1, 2), new ObjectCard(type, 1));
+        s.getGrid().put(new Coordinate(1, 3), new ObjectCard(type, 0));
+        assertEquals(2, s.closeObjectCardsPoints());
+
+        // 4 close object cards
+        s.getGrid().put(new Coordinate(1, 0), new ObjectCard(type, 2));
+        assertEquals(3, s.closeObjectCardsPoints());
+
+        // 5 close object cards
+        s.getGrid().put(new Coordinate(0, 2), new ObjectCard(type, 1));
+        assertEquals(5, s.closeObjectCardsPoints());
+
+        // 6 close object cards
+        s.getGrid().put(new Coordinate(1, 2), new ObjectCard(type, 0));
+        assertEquals(8, s.closeObjectCardsPoints());
+
+        // 7 close object cards
+        s.getGrid().put(new Coordinate(2, 2), new ObjectCard(type, 1));
+        assertEquals(8, s.closeObjectCardsPoints());
+
+        // empty shelf
+        for (int row = 0; row < s.ROWS; row++) {
+            for (int col = 0; col < s.COLUMNS; col++) {
+                s.getGrid().remove(new Coordinate(row, col));
+            }
+        }
+        assertEquals(0, s.closeObjectCardsPoints());
+    }
 }
