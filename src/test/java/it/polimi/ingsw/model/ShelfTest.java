7c16915386b5fc4a002cd57a01220daf21eaaede
matteozamu <matteoz2001@gmail.com>
2023-04-03 23:24:16 +0200

    testCloseObjectCardsPoints

--- src/test/java/it/polimi/ingsw/model/ShelfTest.java
+++ src/test/java/it/polimi/ingsw/model/ShelfTest.java
@@ -31,48 +31,39 @@ public class ShelfTest extends TestCase {
         assertEquals(6-cards, this.s.getAvailableRows(0));
     }
 
-
-
         @Test
-        //TODO: non passa
+        //TODO: da rivedere il test. Il metodo è ok
         public void testCloseObjectCardsPoints() {
-
             // 0 close object cards
             for (int row = 0; row < s.ROWS; row++) {
                 for (int col = 0; col < s.COLUMNS; col++) {
                     s.getGrid().put(new Coordinate(row, col), new ObjectCard(ObjectCardType.randomObjectCardType(), 1));
                 }
             }
-            int points = s.closeObjectCardsPoints();
-            assertEquals(0, points);
+            assertEquals(0, s.closeObjectCardsPoints());
 
             // 3 close object cards
             ObjectCardType type = ObjectCardType.gatto;
             s.getGrid().put(new Coordinate(1, 1), new ObjectCard(type, 2));
             s.getGrid().put(new Coordinate(1, 2), new ObjectCard(type, 1));
             s.getGrid().put(new Coordinate(1, 3), new ObjectCard(type, 0));
-            points = s.closeObjectCardsPoints();
-            assertEquals(2, points);
+            assertEquals(2, s.closeObjectCardsPoints());
 
             // 4 close object cards
             s.getGrid().put(new Coordinate(1, 0), new ObjectCard(type, 2));
-            points = s.closeObjectCardsPoints();
-            assertEquals(3, points);
+            assertEquals(3, s.closeObjectCardsPoints());
 
             // 5 close object cards
             s.getGrid().put(new Coordinate(0, 2), new ObjectCard(type, 1));
-            points = s.closeObjectCardsPoints();
-            assertEquals(5, points);
+            assertEquals(5, s.closeObjectCardsPoints());
 
             // 6 close object cards
             s.getGrid().put(new Coordinate(1, 2), new ObjectCard(type, 0));
-            points = s.closeObjectCardsPoints();
-            assertEquals(8, points);
+            assertEquals(8, s.closeObjectCardsPoints());
 
             // 7 close object cards
             s.getGrid().put(new Coordinate(2, 2), new ObjectCard(type, 1));
-            points = s.closeObjectCardsPoints();
-            assertEquals(8, points);
+            assertEquals(8, s.closeObjectCardsPoints());
 
             // empty shelf
             for (int row = 0; row < s.ROWS; row++) {
@@ -80,21 +71,6 @@ public class ShelfTest extends TestCase {
                     s.getGrid().remove(new Coordinate(row, col));
                 }
             }
-            points = s.closeObjectCardsPoints();
-            assertEquals(0, points);
+            assertEquals(0, s.closeObjectCardsPoints());
         }
-
-
-//    @Test
-//    public void testCheckFull() {
-//        for (int row = 0; row < s.ROWS; row++) {
-//            for (int col = 0; col < s.COLUMNS; col++) {
-//                this.oc = new ObjectCard(ObjectCardType.gatto, 0);
-//                s.getGrid().put(new Coordinate(row, col), this.oc);
-//            }
-//        }
-//        // Verifica che la Shelf sia piena.
-//        assertTrue(s.getFull());
-//    }
-
 }
