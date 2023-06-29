e883830ed074039fa5662df07b9850b5e1be5f50
federicazanchi <federica2.zanchi@mail.polimi.it>
2023-06-29 17:25:27 +0200

    cleaned comments from tests

--- src/test/java/it/polimi/ingsw/model/GameSerializedTest.java
+++ src/test/java/it/polimi/ingsw/model/GameSerializedTest.java
@@ -17,14 +17,8 @@ class GameSerializedTest {
         Game.getInstanceMap().put("username1", g);
         Game.getInstanceMap().put("username2", g);
 
-//        System.out.println(Game.getInstance("username1"));
-
         JsonReader.readJsonConstant("GameConstant.json");
-//        Player player1 = new Player("username1", new Shelf(), new PersonalGoalCard(null, "personalGoalCard-1"));
-//        Player player2 = new Player("username2", new Shelf(), new PersonalGoalCard(null, "personalGoalCard-2"));
-//        ArrayList<Player> players = new ArrayList<>();
-//        players.add(player1);
-//        players.add(player2);
+
         int[][] boardMatrix = JsonReader.getBoard(2);
         GameSerialized gameSerialized = new GameSerialized("username1", "GameConstant.json");
         assertArrayEquals(boardMatrix, gameSerialized.getBoardMatrix());
